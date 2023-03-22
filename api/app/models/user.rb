2edf742124c172da130733bfd94e7235a83f41d3
Issa Nyale <me@email.com>
2023-03-22 15:37:32 +0300

    Created Todo model

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -1,14 +1,14 @@
 class User < ApplicationRecord
     has_secure_password
 
-    validates :username, { 
-        presence: true,
-        length: { minimum: 5, maximum: 8 },
-        uniqueness: true
-    }
+        validates :username, { 
+            presence: true,
+            length: { minimum: 5, maximum: 8 },
+            uniqueness: true
+        }
 
-    validates :email, {
-        uniqueness: true,
-        presence: true
-    }
+        validates :email, {
+            uniqueness: true,
+            presence: true
+        }
 end
