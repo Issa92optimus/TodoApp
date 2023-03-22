4c05cecf6cc5722d3129cbea579fa6a6b3dd29ee
Issa Nyale <me@email.com>
2023-03-22 15:38:38 +0300

    Added functionality to Todo model

--- api/app/models/todo.rb
+++ api/app/models/todo.rb
@@ -1,2 +1,7 @@
 class Todo < ApplicationRecord
+    enum :status, [:CREATED, :IN_PROGRESS, :COMPLETE, :CANCELLED]
+    enum :priority, [:LOW, :MEDIUM, :HIGH]
+
+    validates :title, presence: true, length: { minimum: 8, maximum: 15 }
+    validates :description, presence: true, length: { minimum: 25 }
 end
