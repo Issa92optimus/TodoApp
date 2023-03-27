57e897d34fe0feb60b2cde4e31c62ee54d976b00
Issa Nyale <me@email.com>
2023-03-27 12:54:57 +0300

    Updated App.js

--- API/client/src/component/App.js
+++ API/client/src/component/App.js
@@ -1,1 +1,27 @@
+import React from 'react';
+import Login from './Login';
+import "./App.css";
+import TaskList from './TaskList';
+import TaskForm from './TaskForm';
+import Header from './Header';
+import TaskListContextProvider from '../context/TaskListContext';
 
+
+function App () {
+  return (
+    <TaskListContextProvider>
+      <Login />
+    <div className='container'>
+      <div className='app-wrapper'>
+        <div className='main'>
+          <Header />
+          <TaskForm />
+          <TaskList />
+        </div>
+      </div>
+    </div>
+    </TaskListContextProvider>
+  );
+  }
+
+export default App;
