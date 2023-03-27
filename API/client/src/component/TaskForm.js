34af5ad13b3bf22e47aecdef777f3f6c84433d94
Issa Nyale <me@email.com>
2023-03-27 12:48:49 +0300

    Added functionality to TaskForm

---
+++ API/client/src/component/TaskForm.js
@@ -0,0 +1,56 @@
+import React, {useContext, useState, useEffect} from "react";
+import { TaskListContext } from "../context/TaskListContext";
+
+const TaskForm = () => {
+    const {addTask, clearList, editItem, editTask} = useContext(TaskListContext)
+
+    const [title, setTitle] = useState("")
+
+    const handleChange = e => {
+        setTitle(e.target.value);
+    };
+
+    const handleSubmit = e => {
+        e.preventDefault();
+                fetch('http://127.0.0.1:3000/todo_app', {
+            method: "GET",
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify({
+                title: title,
+            })
+        })
+        .then(resp => resp.json())
+        .then(addTask => {
+            this.props.addTask(addTask)
+        })
+        if(!editItem) {
+        addTask(title);
+        setTitle("");
+        } else {
+            editTask(title, editItem.id)
+        }
+    };
+
+    useEffect(()  => {
+        if(editItem) {
+            setTitle(editItem.title);
+            console.log (editItem);
+        } else {
+            setTitle("");
+        }
+    }, [editItem]);
+
+    return (
+        <form onSubmit={handleSubmit} className="form">
+            <input onChange={handleChange} value={title} type="text" className="task-input" placeholder="Add Task..." required/>
+            <div className="buttons">
+                <button type="submit" class="btn btn-dark me-3" className="btn add-task">{editItem ? 'Edit Task' : 'Add Task'}</button>
+                <button onClick={clearList} class="btn btn-dark" className="btn clear-btn">Clear</button>
+            </div>
+        </form>
+    )
+}
+
+export default TaskForm
